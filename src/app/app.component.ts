abd0d0e8e76a40058baa72326d190f77628c4290
Anh Hoàng Thế <anhhtchy@gmail.com>
2021-06-08 13:08:27 +0700

    getting started with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Task } from './model/task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-list';
+  tasks: Task[] = [
+    {
+      id: 0,
+      title: 'Học HTML',
+    },
+    {
+      id: 1,
+      title: 'Học CSS',
+    },
+    {
+      id: 2,
+      title: 'Học JavaScript',
+    },
+    {
+      id: 3,
+      title: 'Học Angular',
+    },
+  ];
+  doneTask(taskId: number): void {
+    // alert("Done task " + id);
+    const id = this.tasks.findIndex((task) => task.id === taskId);
+    this.tasks.splice(id, 1);
+  }
+  addTask(inputTask: any): void {
+    let maxId = this.tasks.reduce(function (a, b) {
+      return a.id > b.id ? a : b;
+    }).id;
+    this.tasks.push({
+      id: maxId+1,
+      title: inputTask.value,
+    });
+    inputTask.value = '';
+  }
 }
